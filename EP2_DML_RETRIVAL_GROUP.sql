dd6b18ab3952903eb787f7805ea1dcb4992d088f
Matheus Santos <mattsantos.conce@usp.br>
2019-06-02 17:31:48 -0300

    prototipo para a função RETRIVAL

---
+++ EP2_DML_RETRIVAL_GROUP.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+	CREATE OR REPLACE FUNCTION select_pessoa(p_query)
+	RETURNS TABLE (
+		pe_email VARCHAR
+		pe_nusp INT,
+		pe_nome VARCHAR
+	)
+
+	AS $$
+	BEGIN 
+		RETURN QUERY SELECT 
+		pe_email, 
+		cast(pe_nusp as integer), 
+		pe_nome
+
+		FROM
+		PESSOA
+		WHERE
+		pe_nome ILIKE p_query;
+
+	END; $$
+
+	LANGUAGE plpglql;
